15b4d56fe058744ae88762ee3eb42ac1a5f0be4a
sebhastiao-lucio <sebastiao@ice.ufjf.br>
2023-03-03 08:33:17 -0300

    update

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const cluster = require('node:cluster');
 const totalCPUs = require('node:os').cpus().length;
 const process = require('node:process');
 
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+
+} else {
+  startExpress();
+}
+function startExpress() {
 
 
 app.use("/public/TemplateData",express.static(__dirname + "/public/TemplateData"));
@@ -505,3 +524,4 @@ var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
 });
 console.log("------- server is running -------");
+}
