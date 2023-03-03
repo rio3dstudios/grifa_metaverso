8d65cb07d5e344dc63a4a8776b79dd718643ec8c
sebhastiao-lucio <sebastiao@ice.ufjf.br>
2023-03-03 09:04:51 -0300

    update

--- index.js
+++ index.js
@@ -5,18 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/messages', (req, res) => {
-  res.send("ffsd");
-})
-
-app.post('/messages', (req, res) => {
-	console.log(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
-})
 
 io.on('connection', () =>{
   console.log('a user is connected')
